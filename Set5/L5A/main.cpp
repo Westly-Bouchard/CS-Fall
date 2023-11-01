45e3538f38c9c12299719d35a1b94e588be464c8
Westly Bouchard <westly.bouchard@gmail.com>
2023-11-01 15:55:38 -0600

    Refactor not to use smart pointers :(

--- Set5/L5A/main.cpp
+++ Set5/L5A/main.cpp
@@ -3,14 +3,13 @@
 
 #include <cstdlib>
 #include <iostream>
-#include <memory>
 using namespace std;
 
-unique_ptr<ARoom> go_to_next_room() {
+ARoom* go_to_next_room() {
     int randRoomChoice = rand() % 10; // a 10% chance to get directly out
     switch(randRoomChoice) {
-    case 7:     return make_unique<ExitRoom>();
-    default:    return make_unique<GuessTheNumberRoom>();
+    case 7:     return new ExitRoom();
+    default:    return new GuessTheNumberRoom();
     }
 }
 
@@ -18,9 +17,10 @@ int main() {
     srand( time(0) );
     rand();
 
-    unique_ptr<ARoom> currentRoom;
+    ARoom* currentRoom = nullptr;
 
-    do {    
+    do {
+        delete currentRoom;    
         currentRoom = go_to_next_room();
         cout << "Welcome to the " << currentRoom->getRoomName() << endl;
     } while( !currentRoom->escapeTheRoom() );
